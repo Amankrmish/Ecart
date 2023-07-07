22f6ba27f2bc6a7681861472b2e3a1321f327631
Aman Kumar <krishnaman95@gmail.com>
2023-07-07 14:05:40 +0530

    Variable lerning

---
+++ variables/variables.js
@@ -0,0 +1,4 @@
+var firstName = "Aman";
+var LastName = "Kumar";
+// using template variable
+console.log("Hi ".concat(firstName, " ").concat(LastName));
---
+++ variables/variables.ts
@@ -0,0 +1,5 @@
+let firstName:string="Aman"
+let LastName:string="Kumar"
+
+// using template variable
+console.log(`Hi ${firstName} ${LastName}`);
